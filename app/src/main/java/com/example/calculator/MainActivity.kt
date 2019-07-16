5289b0f8f24134a5fb39513ab77fa3b00aa20118
WarkahScott <warkahscott@gmail.com>
2019-07-16 00:24:00 -0400

    Simple L -> R calculations done, need to format the output

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,76 +5,123 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.ArithmeticException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
 
-        var value = 0.0
-        var text = "0"
-        val equation = mutableListOf<String>()
+		var value = 0.0
+		var text = "0"
+		val equation = mutableListOf<String>()
+		val operators = listOf("+", "-", "x", "/")
+		var cleared = false
 
-        fun update(entered : Char)
-        {
-            text = if (!value.isNaN()) text else "0"
-            value = if (!value.isNaN()) value else 0.0
+		fun clear()
+		{
+			input.text = "0"
+			equation.clear()
+			text = "0"
+			value = 0.0
+			cleared = true
+		}
 
-            when(entered)
-            {
-                in '0'..'9' ->
-                {
-                    if(text == "0")
-                    {
-                        if (entered != '0')
-                            text = entered.toString()
-                    }
-                    else
-                        text += entered
-                }
-                '.' ->
-                {
-                    if(text.contains('.'))
+		fun evaluate()
+		{
+			var operation = "+"
+			for (e in equation)
+			{
+				if (e !in operators)
+				{
+					when (operation)
+					{
+						"+" -> value += e.toDouble()
+						"-" -> value -= e.toDouble()
+						"x" -> value *= e.toDouble()
+						"/" -> value /= e.toDouble()
+					}
+				}
+				else
+					operation = e
+			}
+		}
 
-                    else
-                        text += entered
-                }
-                else ->
-                {
-                    equation.add(text)
-                    equation.add(entered.toString())
-                    text = ""
-                }
-            }
+		fun update(entered: Char)
+		{
+			text = if (!cleared) text else "0"
+			cleared = false
+			when (entered)
+			{
+				in '0'..'9' ->
+				{
+					if (text == "0")
+					{
+						if (entered != '0')
+							text = entered.toString()
+					}
+					else
+						text += entered
+				}
+				'.'         ->
+				{
+					if (text.contains('.'))
+					else
+						text += entered
+				}
+				else        ->
+				{
+					if (text != "")
+						equation.add(text.toDouble().toString())
 
-            input.text = if (!value.isNaN()) text else "Err: Zero Divide"
-        }
+					if (equation.isEmpty() || equation.last() in operators)
+					{
+						text = "Error"
+						equation.clear()
+						cleared = true
+					}
+					else if (entered == '=')
+					{
+						evaluate()
+						text = "$value \n $equation"
+						value = 0.0
+						equation.clear()
+						cleared = true
+					}
+					else
+					{
+						equation.add(entered.toString())
+						text = ""
+						println(equation)
+					}
 
-        fun clear()
-        {
-            input.text = "0"
-            text = "0"
-            value = 0.0
-        }
+				}
+			}
 
-        clear.setOnClickListener { clear() }
+			input.text = text
+			text = if (cleared) "" else text
+		}
 
-        zero.setOnClickListener { update('0') }
-        one.setOnClickListener { update('1') }
-        two.setOnClickListener { update('2') }
-        three.setOnClickListener { update('3') }
-        four.setOnClickListener { update('4') }
-        five.setOnClickListener { update('5') }
-        six.setOnClickListener { update('6') }
-        seven.setOnClickListener { update('7') }
-        eight.setOnClickListener { update('8') }
-        nine.setOnClickListener { update('9') }
-        decimal.setOnClickListener { update('.') }
+		clear.setOnClickListener { clear() }
 
-        plus.setOnClickListener { update('+') }
-        minus.setOnClickListener { update('-') }
-        multiply.setOnClickListener { update('x') }
-        divide.setOnClickListener { update('/') }
-        //equals.setOnClickListener{ update('=') }
-    }
+		zero.setOnClickListener { update('0') }
+		one.setOnClickListener { update('1') }
+		two.setOnClickListener { update('2') }
+		three.setOnClickListener { update('3') }
+		four.setOnClickListener { update('4') }
+		five.setOnClickListener { update('5') }
+		six.setOnClickListener { update('6') }
+		seven.setOnClickListener { update('7') }
+		eight.setOnClickListener { update('8') }
+		nine.setOnClickListener { update('9') }
+		decimal.setOnClickListener { update('.') }
+
+		plus.setOnClickListener { update('+') }
+		minus.setOnClickListener { update('-') }
+		multiply.setOnClickListener { update('x') }
+		divide.setOnClickListener { update('/') }
+
+		equals.setOnClickListener { update('=') }
+	}
 }
